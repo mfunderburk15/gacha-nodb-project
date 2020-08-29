2c60be06965fe92eae6b46b4b6e88a124e887dfc
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2020-08-29 15:39:05 -0500

    front end started, header finished

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
 import React from 'react';
+import Display from './components/Display';
+import Header from './components/Header';
 import './App.css';
+import './reset.css';
 
+function App() {
+    return (
+        <div className="App">
+            <Header />
+        </div>
+    )
+}
+
+export default App
